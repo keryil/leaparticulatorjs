4b147c7fbac347388ca62b4ccdf086cb78bf31c7
Kerem Eryilmaz <keryilmaz@gmail.com>
2016-05-20 22:29:31 +0200

    Modularized the theremin.

--- static/js/theremin.js
+++ static/js/theremin.js
@@ -2,48 +2,67 @@
  * Created by Kerem on 19/05/16.
  */
 
-var frequency = 440;
-var synth = new Tone.SimpleSynth().toMaster();
-synth.triggerAttack("C4", "8n");
+var Theremin = (function () {
+    var callbacks = [];
+    var loopingCall = null;
+    var frequency = 440;
+    var synth = new Tone.SimpleSynth().toMaster();
 
-LeapController = new Leap.Controller({
+    var dispatch = function(frame) {
+        for (var i = 0; i < callbacks.length; i++) {
+            callbacks[i](frame);
+        }
+    };
+
+    var addCallback = function(callback) {
+        if(callbacks.indexOf(callback) == -1) {
+            callbacks.push(callback)
+        }
+    };
+
+    var onFrame = function(frame) {
+        if (frame.hands[0]) {
+            var output3 = document.getElementById('output3');
+            // calculate frequency
+            x = frame.hands[0].palmPosition[0];
+            freq = 110 * Math.pow(3, Math.abs(x + 200) / 200);
+
+            // calculate amplitude
+            y = frame.hands[0].palmPosition[1];
+            amp = 1.1 - Math.log(Math.abs(y)) / Math.log(250.);
+            amp = Math.min(1., Math.max(0., amp));
+            amp_db = 20 * (Math.log(amp) / Math.log(10));
+
+            output3.innerHTML = frame.hands[0].palmPosition[0] + ' ' + freq;
+            synth.setNote(freq);
+            synth.volume.value = amp_db;
+            // console.log(synth.volume.value);
+        }
+        else {
+            synth.volume.value = -99;
+        }
+    };
+
+    LeapController = new Leap.Controller({
       frameEventName: 'deviceFrame'
     });
-LeapController.connect();
-var Theremin = function () {
-    this.callbacks = [];
-    this.loopingCall = null;
-}
+    LeapController.connect();
 
-Theremin.prototype.dispatch = function(frame) {
-        for (var i = 0; i < this.callbacks.length; i++) {
-            this.callbacks[i](frame);
-        }
-    };
-Theremin.prototype.addCallback = function(callback) {
-        if(this.callbacks.indexOf(callback) == -1) {
-            this.callbacks.push(callback)
-        }
-    };
-
-
-var theremin = new Theremin();
-LeapController.on("frame", function(frame){
-        theremin.dispatch(frame);
+    LeapController.on("frame", function(frame){
+        dispatch(frame);
     });
+    addCallback(onFrame);
 
-theremin.addCallback(function(frame) {
-    if (frame.hands[0]) {
-        var output3 = document.getElementById('output3');
-        x = frame.hands[0].palmPosition[0];
-        val = 110 * Math.pow(3, Math.abs(x + 200) / 200);
-        output3.innerHTML = frame.hands[0].palmPosition[0] + ' ' + val;
-        synth.setNote(val);
-        synth.volume.value = 0;
-        // console.log(synth.volume);
-    }
-    else {
-        synth.volume.value = -99;
-    }
-});
+    return {
+        dispatch: dispatch,
+        addCallback: addCallback,
+        callbacks: callbacks,
+        synth: synth,
+        frequency: frequency,
+        loopingCall: loopingCall,
+    };
+
+})();
+
+Theremin.synth.triggerAttack("C4", "8n");
 
