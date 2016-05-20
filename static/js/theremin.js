5d0630b272ce4ca489731256c61eecd4af7863ff
Kerem Eryilmaz <keryilmaz@gmail.com>
2016-05-21 00:29:30 +0200

    Various fixes.

--- static/js/theremin.js
+++ static/js/theremin.js
@@ -6,7 +6,34 @@ var Theremin = (function () {
     var callbacks = [];
     var loopingCall = null;
     var frequency = 440;
+    var amp = Math.log(.5);
     var synth = new Tone.SimpleSynth().toMaster();
+    var muted = false;
+    var playing = false;
+
+    var toggleMute = function() {
+        muted = !muted;
+    };
+
+    var isMuted = function() {
+        return muted;
+    }
+
+    var getFrequency = function() {
+        return frequency;
+    }
+
+    var getAmplitude = function() {
+        return amp;
+    }
+
+    var isPlaying = function() {
+        return playing;
+    }
+
+    var to_dB = function(amp) {
+        return 20 * (Math.log(amp) / Math.log(10));
+    }
 
     var dispatch = function(frame) {
         for (var i = 0; i < callbacks.length; i++) {
@@ -16,34 +43,43 @@ var Theremin = (function () {
 
     var addCallback = function(callback) {
         if(callbacks.indexOf(callback) == -1) {
-            callbacks.push(callback)
+            callbacks.push(callback);
+        }
+        else {
+            console.log("Duplicate callback " + callback);
         }
     };
 
     var onFrame = function(frame) {
+        if (muted) {
+            playing = false;
+            synth.triggerRelease();
+            return;
+        }
         if (frame.hands[0]) {
-            var output3 = document.getElementById('output3');
             // calculate frequency
             x = frame.hands[0].palmPosition[0];
-            freq = 110 * Math.pow(3, Math.abs(x + 200) / 200);
+            frequency = 110 * Math.pow(3, Math.abs(x + 200) / 200);
 
             // calculate amplitude
             y = frame.hands[0].palmPosition[1];
             amp = 1.1 - Math.log(Math.abs(y)) / Math.log(250.);
             amp = Math.min(1., Math.max(0., amp));
-            amp_db = 20 * (Math.log(amp) / Math.log(10));
 
-            output3.innerHTML = frame.hands[0].palmPosition[0] + ' ' + freq;
-            synth.setNote(freq);
-            synth.volume.value = amp_db;
-            // console.log(synth.volume.value);
+            synth.setNote(frequency);
+            synth.volume.value = to_dB(amp);
+            if (!playing) {
+                synth.triggerAttack(frequency);
+                playing = true;
+            }
         }
         else {
-            synth.volume.value = -99;
+            synth.triggerRelease();
+            playing = false;
         }
     };
 
-    LeapController = new Leap.Controller({
+    var LeapController = new Leap.Controller({
       frameEventName: 'deviceFrame'
     });
     LeapController.connect();
@@ -58,11 +94,14 @@ var Theremin = (function () {
         addCallback: addCallback,
         callbacks: callbacks,
         synth: synth,
-        frequency: frequency,
+        getFrequency: getFrequency,
         loopingCall: loopingCall,
+        toggleMute:toggleMute,
+        getAmplitude:getAmplitude,
+        isPlaying:isPlaying,
+        isMuted:isMuted
     };
 
-})();
+}());
 
-Theremin.synth.triggerAttack("C4", "8n");
 
